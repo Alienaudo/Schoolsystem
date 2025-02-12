d5c3f38b0a047ee00564c426f26a7a353fe438ee
tiago.veneza <tiago.veneza@ms.sebrae.com.br>
2025-02-12 11:51:21 -0400

    Cria pasta e move arquivos
    Cria pasta api e move arquivos relacionados para ela
    Cria arquivos de rotas da api

--- src/core/api/http/server.ts
+++ src/core/api/http/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
-import { pool } from "../database/connections/creatConnection.js";
+
+import { pool } from '@/database/connections/creatConnection.js';
+
+import { home } from '../routes/routes.js';
+import { registerStudantPOST, studentsRegistered } from '../routes/studantsRoutes.js';
 
 dotenv.config();
 
@@ -9,12 +12,12 @@ const app = express();
 const host: string = process.env.HOST || 'localhost';
 const port: number = parseInt(process.env.PORT_SERVER || '3000', 10);
 
-const studants: string[] = [];
-
 app.use(express.json());
-app.use(express.static('../HTML/styles'));
 
+app.use('/', home);
 
+app.use('/api/registerStudant', registerStudantPOST);
+app.use('/api/studentsRegistered', studentsRegistered);
 
 app.listen(port, host, () => {
 	console.log(`Server running at http://${host}:${port}/`);
