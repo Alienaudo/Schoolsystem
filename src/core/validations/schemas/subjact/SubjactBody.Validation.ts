7798c525c5585280213ef0f385aeb290350fcdc4
tiago.veneza <tiago.veneza@ms.sebrae.com.br>
2025-03-12 08:55:11 -0400

    Cria esquemas de validação
    Eles serão utilizados na hora de fazer as validações

---
+++ src/core/validations/schemas/subjact/SubjactBody.Validation.ts
@@ -0,0 +1,28 @@
+import { ptBrErros } from '../../../services/Translations.service.js'; 
+import { subject } from '../../../interfaces/subject.js'; 
+import Joi from 'joi';
+
+export const bodyValidation: Joi.ObjectSchema<subject> = Joi.object({
+    
+    subjectName: 
+        Joi.string()
+            .required()
+            .min(3)
+            .max(50)
+            .messages(ptBrErros),
+
+    hours:
+        Joi.number()
+            .required()
+            .min(1)
+            .max(3)
+            .messages(ptBrErros),
+
+    description:
+        Joi.string()
+            .required()
+            .min(10)
+            .max(300)
+            .messages(ptBrErros)
+
+});
