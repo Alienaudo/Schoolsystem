6e511beaa5729121cfb41863c2e0770525c79c0c
tiago.veneza <tiago.veneza@ms.sebrae.com.br>
2025-03-18 15:57:10 -0400

    Cria rotas
    Cria as rotas para os endpoints

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import '../config/TranslationsYpu.js';
-import { enrollStudent } from './routes/Studants.Routes.js';   
+import * as a from './routes/Subject.Routes.js';
+import * as b from './routes/Studants.Routes.js';   
+import * as c from './routes/Teachers.Routes.js'
 
 dotenv.config();
 
@@ -12,16 +13,100 @@ const port: number = parseInt(process.env.PORT_SERVER || '3000', 10);
 
 app.use(express.json());
 
-app.route('/').get((_, res) => {
+app.route('/').get((_, res) => { res.send('OK!') });
 
-    res.send('OK!')
+/**
+ * Subjects rotas.
+ */
+app.use('/api/materias', 
+    
+    a.getAllSubjacts,
+    a.registerSubjact,
+    a.updateSubjact,
+    a.removeSubjact
 
-});
+);
 
-app.use('/api/students', enrollStudent);
+app.use('/api/materias/id/:id',
 
-app.listen(port, host, () => {
+    a.getSubjectById
 
+);
+
+/**
+ * Students rotas.
+ */
+app.use('/api/estudantes', 
+
+    b.enrollStudent, 
+    b.getAllStudents
+
+);
+app.use('/api/estudantes/cpf/:cpf', 
+    
+    b.getStudentByCpf,
+    b.updateStudentByCpf,
+    b.removeStudantByCpf
+
+);
+
+app.use('/api/estudantes/materiaid/:id', 
+    
+    b.getStudentById
+
+);
+
+app.use('/api/estudantes/nome/:name', 
+    
+    b.getStudentByName
+
+);
+
+app.use('/api/estudantes/materiaId/:subjactId', 
+    
+    b.getStudentBySubject
+
+);
+
+/**
+ * Teachers rotas.
+ */
+app.use('/api/professores',
+
+    c.getAllTeachers,
+    c.updateTeacher,
+    c.removeTeacherByCpf
+
+);
+
+app.use('/api/professores/id/:id',
+
+    c.getTeacherById
+
+);
+
+app.use('/api/professores/cpf/:cpf',
+
+    c.getTeacherByCpf
+
+);
+
+app.use('/api/professores/nome/:name',
+
+    c.getTeacherByName
+
+);
+
+app.use('/api/professores/materiaId/:subjactId',
+
+    c.getTeacherBySubjactId
+    
+);
+
+app.listen(port, host, (err) => {
+
+    if (err) throw new Error(err.message);
+    
 	console.log(`Server running at http://${host}:${port}/`);
     
 });
